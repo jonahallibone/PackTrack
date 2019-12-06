1bb854b3de58eff1074808b16eb9e246065bbbc1
Jonah Allibone <jonah.allibone@gmail.com>
2019-12-06 01:40:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+/*global chrome*/
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Send message from active tab to background: 
+    chrome.runtime.sendMessage("hello", response => {
+      console.log(`message from background: ${JSON.stringify(response)}`);
+      setMessage(response)
+    });
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message}
     </div>
   );
 }
